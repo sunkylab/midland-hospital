5aca555d8e281cbc38eb551c6eada2a50dc8a3c0
towolabi <towolabi@seamfix.com>
2022-04-03 12:11:17 +0100

    added unit tests
    created integration test

--- src/test/java/com/midland/hospital/HospitalApplicationIntegrationTests.java
+++ src/test/java/com/midland/hospital/HospitalApplicationIntegrationTests.java
@@ -1,48 +1,36 @@
 package com.midland.hospital;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-class HospitalApplicationTests {
+@AutoConfigureMockMvc
+@TestPropertySource(
+		locations = "classpath:application-integrationtest.properties")
+class HospitalApplicationIntegrationTests {
+
+	@Autowired
+	private MockMvc mvc;
 
 
-
-	@Test
-	void shouldHaveUUIDGeneratedWhenStaffIsCreated() {
-
-
-	}
-
-	@Test
-	void shouldUpdateStaffProfileWhenProfileUpdateIsDone() {
-
-	}
-
-	@Test
-	void shouldReturnSuccessWhenValidUUIDisUsedForStaffUpdate() {
-
-	}
-
-	@Test
-	void shouldReturnFailureWhenInvalidUUIDisUsedForStaffUpdate() {
-
-	}
-
-
-	@Test
-	void shouldReturnAllPatientRecordsIfPatientsExist() {
-
-	}
-
-	@Test
-	void shouldReturnPatientRecordsThatMatchFilterOnly() {
-
-	}
-
-	@Test
-	void shouldOnlyUpdateDelFlagWhenPatientDeletionIsDone() {
-
-	}
+//	@Test
+//	public void givenEmployees_whenGetEmployees_thenStatus200()
+//			throws Exception {
+//
+//		mvc.perform(get("/api/employees")
+//				.contentType(MediaType.APPLICATION_JSON))
+//				.andExpect(status().isOk())
+//				.andExpect(content()
+//						.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+//				.andExpect(jsonPath("$[0].name", is("bob")));
+//	}
 
 }
--- src/test/java/com/midland/hospital/HospitalApplicationUnitTests.java
+++ src/test/java/com/midland/hospital/HospitalApplicationUnitTests.java
@@ -1,48 +1,116 @@
 package com.midland.hospital;
 
+import com.midland.hospital.core.exceptions.StaffUpdateException;
+import com.midland.hospital.modules.patient.dto.PatientFilterDTO;
+import com.midland.hospital.modules.patient.dto.PatientProfileDTO;
+import com.midland.hospital.modules.patient.service.PatientService;
+import com.midland.hospital.modules.staff.dto.StaffProfileDTO;
+import com.midland.hospital.modules.staff.service.StaffService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class HospitalApplicationTests {
+import java.util.Date;
+import java.util.List;
 
+@SpringBootTest
+class HospitalApplicationUnitTests {
+
+	@Autowired
+	private StaffService staffService;
+
+	@Autowired
+	private PatientService patientService;
 
 
 	@Test
 	void shouldHaveUUIDGeneratedWhenStaffIsCreated() {
 
+		//Given
+		StaffProfileDTO profileDTO = new StaffProfileDTO();
 
+		//When
+		String uuid = staffService.createStaffRecord(profileDTO);
+
+		//Then
+		Assertions.assertNotNull(uuid);
+		Assertions.assertTrue(uuid.length() == 16);
 	}
 
-	@Test
-	void shouldUpdateStaffProfileWhenProfileUpdateIsDone() {
-
-	}
 
 	@Test
 	void shouldReturnSuccessWhenValidUUIDisUsedForStaffUpdate() {
+		//Given
+		StaffProfileDTO profileDTO = new StaffProfileDTO();
+		profileDTO.setName("James");
 
+		//When
+		String uuid = staffService.createStaffRecord(profileDTO);
+		profileDTO.setName("Sam");
+		staffService.updateStaffRecord(profileDTO,uuid);
+		StaffProfileDTO updatedRecord = staffService.getStaffRecord(uuid);
+
+		//Then
+		Assertions.assertEquals(updatedRecord.getName(),profileDTO.getName());
+		Assertions.assertDoesNotThrow(() -> StaffUpdateException.class);
 	}
 
 	@Test
 	void shouldReturnFailureWhenInvalidUUIDisUsedForStaffUpdate() {
 
+		//Given
+		StaffProfileDTO profileDTO = new StaffProfileDTO();
+		profileDTO.setName("James");
+
+		//When
+		staffService.createStaffRecord(profileDTO);
+
+		//Then
+		Assertions.assertThrows(StaffUpdateException.class,() -> staffService.updateStaffRecord(profileDTO,"uuid"));
 	}
 
 
 	@Test
 	void shouldReturnAllPatientRecordsIfPatientsExist() {
+		//Given
+		PatientFilterDTO noFilter = null;
 
+		//When
+		List<PatientProfileDTO> dtoList = patientService.getPatients(noFilter);
+
+		//Then
+		Assertions.assertEquals(dtoList.size(),100);
 	}
 
 	@Test
 	void shouldReturnPatientRecordsThatMatchFilterOnly() {
+		//Given
+		PatientFilterDTO ageFilter = new PatientFilterDTO(2);
+		PatientFilterDTO dateRangeFilter = new PatientFilterDTO(new Date(),new Date());
 
+		//When
+		List<PatientProfileDTO> dtoList = patientService.getPatients(ageFilter);
+		List<PatientProfileDTO> dtoList2 = patientService.getPatients(dateRangeFilter);
+
+		//Then
+		Assertions.assertEquals(dtoList.size(),20);
+		Assertions.assertEquals(dtoList2.size(),50);
 	}
 
 	@Test
 	void shouldOnlyUpdateDelFlagWhenPatientDeletionIsDone() {
+		//Given
+		PatientFilterDTO noFilter = null;
 
+		//When
+		patientService.removePatients(noFilter);
+		List<PatientProfileDTO> dtoList = patientService.getPatients(noFilter);
+		Long actualDataCount = patientService.getNumberOfRows();
+
+		//Then
+		Assertions.assertEquals(dtoList.size(),0);
+		Assertions.assertEquals(actualDataCount,100);
 	}
 
 }
